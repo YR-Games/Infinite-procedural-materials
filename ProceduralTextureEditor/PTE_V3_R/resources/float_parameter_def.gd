0a8cd0be591682d7e6b2f1c52ad6fb5bdfc942af
Yaros-Lav <Yaros1113@gmail.com>
2026-05-28 18:36:26 +1000

    + вращатель параметров
     * Тегом ??? обозначено место выбора метода обхода, пока добавлены 2 самых простых и элементарнейший механизм прерывания, который останавливает перебор, как только все параметры дойдут до конца, игнорируя множество допустимых комбинаций...

--- ProceduralTextureEditor/PTE_V3_R/resources/float_parameter_def.gd
+++ ProceduralTextureEditor/PTE_V3_R/resources/float_parameter_def.gd
@@ -6,3 +6,6 @@ extends abstract_parameter_def
 @export var min_value: float = 0.0
 @export var max_value: float = 1.0
 @export var step: float = 0.01
+
+func _to_string() -> String:
+	return "{"+param_name +" val="+ str(default_value) + ", ("+ str(min_value) + ", " +str(max_value)+"), "+str(step)+"}"
--- ProceduralTextureEditor/PTE_V3_R/resources/int_parameter_def.gd
+++ ProceduralTextureEditor/PTE_V3_R/resources/int_parameter_def.gd
@@ -6,3 +6,6 @@ extends abstract_parameter_def
 @export var min_value: int = 0
 @export var max_value: int = 1
 @export var step: int = 1
+
+func _to_string() -> String:
+	return "{"+param_name +" val="+ str(default_value) + ", ("+ str(min_value) + ", " +str(max_value)+"), "+str(step)+"}"
